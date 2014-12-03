b9553d07ce1bbc29bb1fdbadebc3e52164c1240f
Thibaud Kehler <Thibaud.Kehler@gmx.net>
2014-12-03 17:20:22 +0100

    Step 1: template, expressions, simple directives

--- app/scripts/controllers/shop.js
+++ app/scripts/controllers/shop.js
@@ -8,59 +8,34 @@
  * Controller of the edgyApp
  */
 angular.module('edgyApp')
-.filter('object2Array', function(){
-    return function(input) {
-      var out = []; 
-      for(var i in input){
-        out.push(input[i]);
-      }
-      return out;
-    };
-})
-.controller('ShopCtrl', function ($scope, $http, $filter, ngDialog) {
-    $http
-    	.get('data/items.json')
-    	.then(function(res){
-    		$scope.items = res.data;                
-        });
+.controller('ShopCtrl', function ($scope, $http) {
 
-    $scope.reverse = false;
-    $scope.order = function(){
-    	$scope.items = $filter('orderBy')($scope.items, 'id', $scope.reverse);
-        $scope.reverse = !($scope.reverse);
-    };
-
-    $scope.cart = [];
-  	$scope.tempProduct = {};
-    $scope.addToCart = function(product){
-    	$scope.tempProduct.name = product.name;
-    	$scope.tempProduct.price = product.price;
-    	$scope.cart.push($scope.tempProduct);
-    	$scope.tempProduct = {};
-    };
-    $scope.removeFromCart = function(product){
-    	$scope.cart.pop(product);
-    };
-
-    $scope.shipping = false;
-    $scope.shippingCost = 3.5;
-    $scope.total = function(){
-    	var res = 0;
-        $scope.shipping = false;
-    	for(var i = 0; i< $scope.cart.length; i++){
-    		res += $scope.cart[i].price;
-    	}
-        if(res < 20 && $scope.cart.length > 0){
-            $scope.shipping = true;
-            res += $scope.shippingCost;
-        }
-    	return res;
-    };
-
-    $scope.go = function() {
-        if($scope.total() > 0){
-            $scope.cart = [];
-            ngDialog.open({template: 'views/success.html'});
-        }
-    };
+    $scope.items = [{
+        "id": 2,
+        "name": "Red Towel",
+        "desc": "Should not be trusted around hairdryers and bathtubs.",
+        "price": 2.45,
+        "inStock": true
+    },
+    {
+        "id:": 3,
+        "name": "Green Towel",
+        "desc": "A towel so green, it drives a Toyota Prius and occasionally holds speaks about the green house effect.",
+        "price": 3.95,
+        "inStock": true
+    },
+    {
+        "id": 1,
+        "name": "Yellow Towel",
+        "desc": "A yellow towel. Nothing to see here.",
+        "price": 0,
+        "inStock": true
+    },
+    {
+        "id": 200,
+        "name": "Not a Towel",
+        "desc": "Something that obviously is not a towel.",
+        "price": 2.20,
+        "instock": false
+    }]               
 });
